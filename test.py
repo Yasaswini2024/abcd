ca54cb3bbf3d3a3a49cb691a3768e75d2dbe9789
Yasaswini2024 <165940251+Yasaswini2024@users.noreply.github.com>
2024-04-16 21:04:16 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# File contains random libraries
+
+import argparse
+import paytmupipayments
+import termcolor
+import requests
+import upipayments
+import sonyflake
+
+def main():
+  print("Hello World!")
+
+main()
